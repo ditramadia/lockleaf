d2a5ee8dd63ea96b3a7b4a5a1dd9d500f7a56f1f
Ditra Amadia <ditrarizqaamadia@gmail.com>
2026-01-24 09:57:07 +0700

    test: add unit tests for add credential and get field

--- internal/vault/manager.go
+++ internal/vault/manager.go
@@ -2,27 +2,35 @@ package vault
 
 import (
 	"errors"
-	"fmt"
+	"strings"
 )
 
 var (
 	ErrCredentialNotFound = errors.New("credential not found")
 	ErrCredentialExists   = errors.New("credential already exists")
+	ErrFieldNotFound      = errors.New("field not found")
 )
+
+// ========================================================================
+// Credential Operation
+// ========================================================================
 
 // AddCredential handles the logic of adding a credential to the vault
 func (v *Vault) AddCredential(name string, fields map[string]Field) error {
+	credentialKey := strings.ToLower(name)
+
+	// Create new credentials if it doesn't exist yet
 	if v.Credentials == nil {
 		v.Credentials = make(map[string]Credential)
 	}
 
 	// Validate if credential with the same name already exists
-	if _, exists := v.Credentials[name]; exists {
+	if _, exists := v.Credentials[credentialKey]; exists {
 		return ErrCredentialExists
 	}
 
 	// Create Credential data
-	v.Credentials[name] = Credential{
+	v.Credentials[credentialKey] = Credential{
 		Name:   name,
 		Fields: fields,
 	}
@@ -30,18 +38,25 @@ func (v *Vault) AddCredential(name string, fields map[string]Field) error {
 	return nil
 }
 
+// ========================================================================
+// Field Operation
+// ========================================================================
+
 // GetField retrieves a specific field value
 func (v *Vault) GetField(credName, fieldName string) (string, error) {
+	credentialKey := strings.ToLower(credName)
+	fieldKey := strings.ToLower(fieldName)
+
 	// Validate if the credential exists
-	cred, ok := v.Credentials[credName]
+	cred, ok := v.Credentials[credentialKey]
 	if !ok {
 		return "", ErrCredentialNotFound
 	}
 
 	// Validate if the field exists
-	field, ok := cred.Fields[fieldName]
+	field, ok := cred.Fields[fieldKey]
 	if !ok {
-		return "", fmt.Errorf("field '%s' not found", fieldName)
+		return "", ErrFieldNotFound
 	}
 
 	return field.Value, nil
