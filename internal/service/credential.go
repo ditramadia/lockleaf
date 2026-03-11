8c792d495998b14995fae929fd1dc3e64e67dcb4
Ditra Amadia <ditrarizqaamadia@gmail.com>
2026-03-12 05:35:57 +0700

    feat (credential): add rename credential service

--- internal/service/credential.go
+++ internal/service/credential.go
@@ -49,3 +49,53 @@ func (s *Service) ListCredentials(vaultName string) ([]string, error) {
 	}
 	return credentials, nil
 }
+
+func (s *Service) RenameCredential(vaultName, name, newName string) error {
+
+	// Check if vault exists
+	isVaultExist, err := s.Storage.IsVaultExist(vaultName)
+	if err != nil {
+		return fmt.Errorf("Error checking vault existance: %w", err)
+	}
+	if !isVaultExist {
+		return fmt.Errorf("Vault '%s' does not exist", vaultName)
+	}
+
+	// Load the vault
+	v, err := s.Storage.Load(vaultName)
+	if err != nil {
+		return fmt.Errorf("Error loading vault: %w", err)
+	}
+
+	// Check if credential exists
+	isCredentialExist, err := s.Storage.IsCredentialExist(v, name)
+	if err != nil {
+		return fmt.Errorf("Error checking credential existance: %w", err)
+	}
+	if !isCredentialExist {
+		return fmt.Errorf("Credential '%s' does not exist", name)
+	}
+
+	// Check if credential with newName already exists
+	isNewCredentialExist, err := s.Storage.IsCredentialExist(v, newName)
+	if err != nil {
+		return fmt.Errorf("Error checking new credential existance: %w", err)
+	}
+	if isNewCredentialExist {
+		return fmt.Errorf("Credential '%s' already exists", newName)
+	}
+
+	// Rename the credential
+	credential := v.Credentials[name]
+	delete(v.Credentials, name)
+	credential.Name = newName
+	v.Credentials[newName] = credential
+
+	// Save the updated vault
+	if err := s.Storage.Save(v); err != nil {
+		return fmt.Errorf("Error saving vault: %w", err)
+	}
+
+	return nil
+
+}
