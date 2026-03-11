431790c29aad86d3c430ef993b3f696a9cecb80e
Ditra Amadia <ditrarizqaamadia@gmail.com>
2026-03-11 07:00:44 +0700

    feat (credential): register add credential command

---
+++ cmd/cred.go
@@ -0,0 +1,22 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var credCmd = &cobra.Command{
+	Use:     "cred",
+	Aliases: []string{"c"},
+	Short:   "Manage credentials within the active vault",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// Add a new credential
+		if len(args) == 1 {
+			credentialName := args[0]
+			h.AddCredential(credentialName)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(credCmd)
+}
